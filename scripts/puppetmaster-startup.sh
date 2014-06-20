97b35d51b7416400d6cd8b69e85e5c39f783f011
Naftuli Tzvi Kay <rfkrocktk@gmail.com>
2014-06-20 13:35:11 -0700

    Environments working with base module path.
    Providing by default the production environment. Everything is now
    living in /data with proper permissions. I don't know if I like the
    sticky bit and if its necessary but it's working. I also have a hanging
    /data/manifests directory which I don't like.
    
    Tested and working with local Puppet Agent.

--- scripts/puppetmaster-startup.sh
+++ scripts/puppetmaster-startup.sh
@@ -30,7 +30,7 @@ if [ ! -z "$PUPPETMASTER_DEBUG" ]; then
 fi
 
 # set the configuration directory and no daemonize
-puppet_master_args="$puppet_master_args --confdir /data/ --environmentpath /data/environments/ --no-daemonize"
+puppet_master_args="$puppet_master_args --confdir /data/ --environmentpath /data/environments/ --basemodulepath /data/modules/ --no-daemonize"
 
 # set port
 puppet_master_args="$puppet_master_args --masterport $PUPPETMASTER_TCP_PORT"
@@ -38,17 +38,23 @@ puppet_master_args="$puppet_master_args --masterport $PUPPETMASTER_TCP_PORT"
 # if /data/puppet.conf doesn't exist, copy over all of the default configuration
 if [ ! -f "/data/puppet.conf" ]; then
     cp -r /etc/puppet/* /data
+    rm -fr /data/manifests # not interested in manifests, we're using environments
+    rm -fr /data/environments/example_env # not interested
 fi
 
 # we want /data to be owned by root
 test -d /data || mkdir /data
 chown root:root /data
 
+# if there's no production environment, create it
+test -d /data/environments/production || mkdir -p /data/environments/production/{manifests,modules}
+test -d /data/modules || mkdir /data/modules
+
 # only the puppet user can read/write/execute things in here
-chmod 7770 -R /data/environments /data/manifests /data/modules
+chmod 7770 -R /data/environments /data/modules
 
 # we want environments to be owned by puppet
-chown puppet:puppet -R /data/environments /data/manifests /data/modules
+chown puppet:puppet -R /data/environments /data/modules
 
 # only root can do important things in /data
 chmod 7775 /data
