f0e204c8190169f4f7bfdc36e5cd1e62fefb0da9
Naftuli Tzvi Kay <rfkrocktk@gmail.com>
2014-06-20 11:01:44 -0700

    Require latest Puppet client on Vagrant box.

--- manifests/vagrant.pp
+++ manifests/vagrant.pp
@@ -2,7 +2,8 @@
 node 'vagrant-precise64' {
     # apt
     class { 'apt': }
-    
+   
+    # docker
     apt::source { 'docker':
         location => "http://get.docker.io/ubuntu",
         key => "36A1D7869245C8950F966E92D8576A8BA88D21E9",
@@ -13,5 +14,17 @@ node 'vagrant-precise64' {
 
     package { 'lxc-docker':
         require => [Apt::Source["docker"]]
+    }
+    
+    # install puppet client in vagrant
+    apt::source { 'puppetlabs':
+        key      => '4BD6EC30',
+        location => 'http://apt.puppetlabs.com',
+        repos    => 'main dependencies'
+    }
+
+    package { 'puppet':
+        ensure  => latest,
+        require => [Apt::Source["puppetlabs"]]
     }
 }
