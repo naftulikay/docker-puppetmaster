51607d36c1a2836103f452b1b7bb259170d1c0bf
Naftuli Tzvi Kay <rfkrocktk@gmail.com>
2014-07-31 10:12:25 -0700

    Environments/manifest mode now possible.
    Defined a new environment variable `PUPPETMASTER_ENVIRONMENTS_ENABLED`
    which will switch the initial configuration of Puppet to environments
    mode. By default, we'll default over to the deprecated manifests mode,
    but when this variable is defined, we'll use Puppet directory
    environments for nodes. This has been tested locally and it works for
    both modes.
    
    Removed the old Puppet Master startup script, as we're on NGINX now.

--- scripts/10_generate_puppet_config.rb
+++ scripts/10_generate_puppet_config.rb
@@ -11,19 +11,20 @@ File.readlines("/etc/container_environment.sh").each do |line|
 end
 
 # setup variables
-puppet_conf_dir          = "/etc/puppet"
-puppet_conf_defaults_dir = "/usr/lib/puppet/default"
-puppet_auth_file         = "#{puppet_conf_dir}/auth.conf"
-puppet_fileserver_file   = "#{puppet_conf_dir}/fileserver.conf"
-puppet_conf_file         = "#{puppet_conf_dir}/puppet.conf"
-puppet_environments_dir  = "#{puppet_conf_dir}/environments"
-puppet_manifests_dir     = "#{puppet_conf_dir}/manifests"
-puppet_modules_dir       = "#{puppet_conf_dir}/modules"
-puppet_templates_dir     = "#{puppet_conf_dir}/templates"
+puppet_conf_dir                   = "/etc/puppet"
+puppet_conf_defaults_dir          = "/usr/lib/puppet/default"
+puppet_auth_file                  = "#{puppet_conf_dir}/auth.conf"
+puppet_fileserver_file            = "#{puppet_conf_dir}/fileserver.conf"
+puppet_conf_file                  = "#{puppet_conf_dir}/puppet.conf"
+puppet_environments_dir           = "#{puppet_conf_dir}/environments"
+puppet_manifests_dir              = "#{puppet_conf_dir}/manifests"
+puppet_modules_dir                = "#{puppet_conf_dir}/modules"
+puppet_templates_dir              = "#{puppet_conf_dir}/templates"
 
-puppetmaster_port        = ENV.fetch("PUPPETMASTER_TCP_PORT", "8140")
-puppetmaster_verbose     = ENV.fetch("PUPPETMASTER_VERBOSE", nil)
-puppetmaster_debug       = ENV.fetch("PUPPETMASTER_DEBUG", nil)
+puppetmaster_port                 = ENV.fetch("PUPPETMASTER_TCP_PORT", "8140")
+puppetmaster_verbose              = ENV.fetch("PUPPETMASTER_VERBOSE", nil)
+puppetmaster_debug                = ENV.fetch("PUPPETMASTER_DEBUG", nil)
+puppetmaster_environments_enabled = ENV.fetch("PUPPETMASTER_ENVIRONMENTS_ENABLED", nil)
 
 passenger_conf_file      = "/usr/share/puppet/rack/puppetmaster/config.ru" 
 
@@ -44,22 +45,27 @@ require 'puppet/util/command_line'
 run Puppet::Util::CommandLine.new.execute
 }).result(binding)
 
-puppet_config_template = %{[main]
+puppet_config_template = ERB.new(%{[main]
 logdir=/var/log/puppet
 vardir=/var/lib/puppet
 ssldir=/var/lib/puppet/ssl
 rundir=/var/run/puppet
 factpath=$vardir/lib/facter
-
+basemodulepath=$confdir/modules
+<% if puppetmaster_environments_enabled %>
+environmentpath=$confdir/environments/
+<% else %>
+manifest=$confdir/manifests/
+<% end %>
 [master]
-masterport = #{puppetmaster_port}
+masterport = <%= puppetmaster_port %>
 ssl_client_header = HTTP_X_CLIENT_S_DN
 ssl_client_verify_header = HTTP_X_CLIENT_VERIFY
 
 [agent]
-server = #{hostname}
-masterport = #{puppetmaster_port}
-}
+server = <%= hostname %>
+masterport = <%= puppetmaster_port %>
+}).result(binding)
 
 # create the root puppet conf dir, this should already be here
 Dir.mkdir(puppet_conf_dir) unless File.directory?(puppet_conf_dir)
--- scripts/12_generate_puppetmaster_keys.sh
+++ scripts/12_generate_puppetmaster_keys.sh
@@ -13,5 +13,5 @@ test -z "$PUPPETMASTER_DNS_NAMES" && \
 
 # if there's no certificate yet, generate it
 if [ ! -f "/var/lib/puppet/ssl/certs/$hostname.pem" ]; then 
-    puppet cert generate --dns_alt_names "$PUPPETMASTER_DNS_NAMES" $fqdn >/dev/null
+    puppet cert generate --dns_alt_names "$PUPPETMASTER_DNS_NAMES" $fqdn >/dev/null 2>&1
 fi
